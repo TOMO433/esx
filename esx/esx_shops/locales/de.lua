1c5fddf5fd6a96b2ae6eb4e24bd8d805cecebd6f
TOMO433 <brajkovictomi1@gmail.com>
2022-02-06 22:30:10 +0100

    esx pack

---
+++ esx/esx_shops/locales/de.lua
@@ -0,0 +1,12 @@
+Locales['de'] = {
+  ['shop'] = 'geschäft',
+  ['shops'] = 'geschäfte',
+  ['press_menu'] = 'drücke ~INPUT_CONTEXT~ um auf das Geschäft zuzugreifen.',
+  ['shop_item'] = '$%s',
+  ['bought'] = 'Du hast ~y~%sx~s~ ~b~%s~s~ für ~r~$%s~s~ Gekauft',
+  ['not_enough'] = 'du ~r~hast nicht~s~ genug geld: %s',
+  ['player_cannot_hold'] = 'Du hast nicht genug Platz im Inventar!',
+  ['shop_confirm'] = 'Kaufe %sx %s für  $%s?',
+  ['no'] = 'Nein',
+  ['yes'] = 'Ja',
+}
---
+++ esx/esx_weaponshop/locales/de.lua
@@ -0,0 +1,13 @@
+Locales ['de'] = {
+  ['buy_license'] = 'Kaufe Waffenlizenz?',
+  ['yes'] = '%s',
+  ['no'] = 'Nein',
+  ['weapon_bought'] = 'Gekauft für %s EUR',
+  ['not_enough_black'] = 'du hast nicht genug Schwarzgeld',
+  ['not_enough'] = 'du hast nicht genug Geld',
+  ['already_owned'] = 'Du besitzt die Waffe bereits!',
+  ['shop_menu_title'] = 'geschäft',
+  ['shop_menu_prompt'] = 'drücke ~INPUT_CONTEXT~ um auf das Geschäft zuzugreifen.',
+  ['shop_menu_item'] = '%s EUR',
+  ['map_blip'] = 'waffenladen',
+}
